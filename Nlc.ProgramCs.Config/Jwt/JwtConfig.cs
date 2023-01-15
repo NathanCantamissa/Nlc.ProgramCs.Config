0b8a33e754e10cad0154ef2261a7e3aad245f166
Nathan Leal Cantamissa <nathan.nsc2@gmail.com>
2023-01-15 20:32:41 -0300

    Adicionar arquivos de projeto.

---
+++ Nlc.ProgramCs.Config/Jwt/JwtConfig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Nlc.ProgramCs.Config.Jwt
+{
+    public static class JwtConfig
+    {
+        /// <summary>
+        /// Easy way to configure Jwt on your application. Just create a json object inside your appsettings like dedscribed bellow.
+        /// "Jwt": {
+        /// "Key": "your_issuer_signing_key",
+        /// "Issuer": "your_isuer",
+        /// "Audience": "your_audience"
+        /// }
+        /// </summary>
+        /// <param name="services">Just to set the extension method</param>
+        /// <param name="configuration">Just pass builder.Configuration, to get the info from appsettings</param>
+        public static void AddJwtConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
+                };
+            });
+        }
+    }
+}
---
+++ Nlc.ProgramCs.Config/Swagger/SwaggerConfig.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Nlc.ProgramCs.Config.Swagger
+{
+    public static class SwaggerConfig
+    {
+        /// <summary>
+        /// Easy way to config the swagger options, with bearer authentication.
+        /// </summary>
+        /// <param name="services">Just to set the extension method</param>
+        /// <param name="name">SwaggerDoc name parameter</param>
+        /// <param name="openApiInfo">OpenApiInfo class, to set the parameters as you desire</param>
+        public static void AddSwaggerBearerConfig(this IServiceCollection services, string name, OpenApiInfo openApiInfo)
+        {
+            services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc(name, openApiInfo);
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer 12345abcdef')",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new string[]{}
+                    }
+                });
+            });
+        }
+    }
+}
